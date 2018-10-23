8bf4b9d3262ed838788e2db74232cec9dd644c87
John-Clapham <jclapham@mail.bradley.edu>
2018-10-22 23:11:24 -0500

    setup
    setup again

--- ECE441_Proj_Clapham.m
+++ ECE441_Proj_Clapham.m
@@ -2,3 +2,13 @@
 %DC Motor position control
 %John_Clapham 10/22/18
 
+b = 0.028;
+j = 1.13*10^-2;
+r = 0.45;
+k = 0.067;
+
+temp = (b+((k^2)/r));
+temp1 = b*j;
+
+%alex likes gitextensions too much
+
