29726609ed4863f3aded6b1d34c7bc4bfc630c51
John-Clapham <jclapham@mail.bradley.edu>
2018-10-25 22:18:03 -0500

    step and impulse response
    ^

--- ECE441_Proj_Clapham.m
+++ ECE441_Proj_Clapham.m
@@ -1,3 +1,5 @@
+clc
+clear all
 %ECE441_Comp_Proj
 %DC Motor position control
 %John_Clapham 10/22/18
@@ -6,9 +8,21 @@ b = 0.028;
 j = 1.13*10^-2;
 r = 0.45;
 k = 0.067;
+kp = 217612;
+kd = 1692;
 
-temp = (b+((k^2)/r));
-temp1 = b*j;
+temp = (b+(((k^2)+(kd*k))/r));
+numerator1 = k/r;
 
+sys = tf([(kd*k/r) ((kp*k)/r)],[j temp (kp*k)/r]);
+
+figure
+rlocus(sys)
+
+figure
+subplot(211)
+impulse(sys)
+subplot(212)
+step(sys)
 %alex likes gitextensions too much
 
